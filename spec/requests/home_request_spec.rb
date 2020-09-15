123fee023279735325c47e0c1c689fb36e537d95
Tran Tuan Anh <yorkittran@gmail.com>
2020-09-15 16:43:14 +0700

    Comment pending example test

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
-  describe 'GET /' do
+  describe 'GET /index' do
     it 'returns http success' do
-      get '/'
+      get '/home/index'
       expect(response).to have_http_status(:success)
     end
   end
