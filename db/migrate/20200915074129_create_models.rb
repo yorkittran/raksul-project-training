3d0dd3b7bab48b4bcbb95bc24a70eab75159e10c
Tran Tuan Anh <yorkittran@gmail.com>
2020-09-16 10:26:15 +0700

    Update

--- db/migrate/20200915074129_create_models.rb
+++ db/migrate/20200915074129_create_models.rb
@@ -2,7 +2,7 @@ class CreateModels < ActiveRecord::Migration[6.0]
   def change
     create_table :models do |t|
       t.string :name
-      t.references :manufacturer, null: false, foreign_key: true
+      t.references :manufacturer, null: false, foreign_key: {on_delete: :cascade}
       t.datetime :year_of_manufacture, null: false
       t.boolean :delete_flag, null: false, default: 0
 
--- db/migrate/20200915080305_create_phones.rb
+++ db/migrate/20200915080305_create_phones.rb
@@ -1,11 +1,11 @@
 class CreatePhones < ActiveRecord::Migration[6.0]
   def change
     create_table :phones do |t|
-      t.references :body_color, null: false, foreign_key: true
-      t.references :memory, null: false, foreign_key: true
-      t.references :os_version, null: false, foreign_key: true
-      t.references :model, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :body_color, null: false, foreign_key: {on_delete: :cascade}
+      t.references :memory, null: false, foreign_key: {on_delete: :cascade}
+      t.references :os_version, null: false, foreign_key: {on_delete: :cascade}
+      t.references :model, null: false, foreign_key: {on_delete: :cascade}
+      t.references :user, null: false, foreign_key: {on_delete: :cascade}
       t.boolean :delete_flag, null: false, default: 0
 
       t.timestamps
