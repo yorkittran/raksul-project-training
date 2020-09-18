d77ec3245eee2d074b797d558f30d1b2121457f6
Tran Tuan Anh <yorkittran@gmail.com>
2020-09-18 09:12:35 +0700

    Add datatable to list model

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,6 @@
 class Model < ApplicationRecord
   belongs_to :manufacturer
+  belongs_to :os_name
+  delegate :name, to: :manufacturer, prefix: true
+  delegate :name, to: :os_name, prefix: true
 end
