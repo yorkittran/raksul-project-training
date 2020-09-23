fb85761d617073a6369ff2dccb147940539f73b8
Tran Tuan Anh <yorkittran@gmail.com>
2020-09-23 11:32:24 +0700

    Config locale

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,11 @@ module RaksulProjectTraining
     config.generators do |g|
       g.template_engine :slim
     end
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :models do
-    member do
-      put 'restore'
+  scope '(:locale)', locale: /en|vi/ do
+    resources :models do
+      member do
+        put 'restore'
+      end
     end
-  end
-  resources :inventories do
-    member do
-      put 'restore'
+    resources :inventories do
+      member do
+        put 'restore'
+      end
     end
+    resources :phones
+    resources :manufacturers
+    resources :os_names
+    resources :body_colors
+    resources :memories
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+    }
+    root 'home#index'
   end
-  resources :phones
-  resources :manufacturers
-  resources :os_names
-  resources :body_colors
-  resources :memories
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-  }
-  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
