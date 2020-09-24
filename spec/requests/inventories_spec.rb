7fcfc7d3c246a80cb85014efa30ac6e29fc1fa07
Tran Tuan Anh <yorkittran@gmail.com>
2020-09-24 15:50:48 +0700

    Fix rubocop

--- spec/requests/inventories_spec.rb
+++ spec/requests/inventories_spec.rb
@@ -7,24 +7,21 @@ RSpec.describe "/inventories", type: :request do
   let(:model) { create(:model, manufacturer_id: manufacturer.id, os_name_id: os_name.id) }
   let(:body_color) { create(:body_color) }
 
-  let(:inventory_with_exists_foreign_key) {{
-    model: { name: { '0': model.name } },
-    body_color: { name: { '0': body_color.name } },
-    memory: { display_name: { '0': '128GB' }, amount: { '0': 128 } },
-    os_version: { major: { '0': 10 }, minor: { '0': 1 }, patch: { '0': 3 } },
-    inventory: { quantity: { '0': 10 }, price: { '0': 200 } }
-  }}
-
-  let(:invalid_attributes) do
-    skip("Add a hash of attributes invalid for your model")
-  end
-
   describe "POST /create" do
+    before do
+      sign_in user
+    end
+
     context "with valid parameters" do
       it "creates a new Inventory with exists foreign key" do
-        sign_in user
-
         expect do
+          inventory_with_exists_foreign_key = {
+            model: { name: { '0': model.name } },
+            body_color: { name: { '0': body_color.name } },
+            memory: { display_name: { '0': '128GB' }, amount: { '0': 128 } },
+            os_version: { major: { '0': 10 }, minor: { '0': 1 }, patch: { '0': 3 } },
+            inventory: { quantity: { '0': 10 }, price: { '0': 200 } },
+          }
           post inventories_url, params: inventory_with_exists_foreign_key
         end.to change(Inventory, :count).by(1)
       end
