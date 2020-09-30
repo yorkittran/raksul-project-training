b83bad69b052b5fdb819dbc1b4f3a5a9027d522d
Tran Tuan Anh <yorkittran@gmail.com>
2020-09-30 14:07:24 +0700

    Validate integer for memory and model

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,4 +1,5 @@
 class Memory < ApplicationRecord
   validates :amount, presence: true
   validates :display_name, presence: true
+  validates :amount, numericality: { only_integer: true }
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -3,5 +3,8 @@ class Model < ApplicationRecord
   belongs_to :os_name
   has_many :phones, dependent: :destroy
 
+  validates :year_of_manufacture, numericality: { only_integer: true }
+
   include Discard::Model
 end
+
